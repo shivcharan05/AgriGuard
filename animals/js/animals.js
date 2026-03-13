f2209bb8b2425f6cd0cf0898830df11d64db6591
Shriraj <shrirajpatil17@gmail.com>
2026-03-13 13:30:02 +0530

    Added Animal Health Management System and CSS styling

--- animals/js/animals.js
+++ animals/js/animals.js
@@ -1,13 +1,7 @@
-let animals = JSON.parse(localStorage.getItem("animals")) || [];
-
-let list = document.getElementById("animalList");
-
-animals.forEach(animal => {
-
-let card = document.createElement("div");
+card.className="animal-card";
 
 card.innerHTML = `
-<img src="${animal.image}" width="120">
+<img src="${animal.image}">
 <h3>${animal.name}</h3>
 <p>Breed: ${animal.breed}</p>
 <p>Age: ${animal.age}</p>
@@ -15,27 +9,3 @@ card.innerHTML = `
 <button onclick="viewAnimal(${animal.id})">View</button>
 <button onclick="deleteAnimal(${animal.id})">Delete</button>
 `;
-
-list.appendChild(card);
-
-});
-
-function viewAnimal(id){
-
-localStorage.setItem("selectedAnimal", id);
-
-window.location.href = "animal-details.html";
-
-}
-
-function deleteAnimal(id){
-
-let animals = JSON.parse(localStorage.getItem("animals"));
-
-animals = animals.filter(a => a.id !== id);
-
-localStorage.setItem("animals", JSON.stringify(animals));
-
-location.reload();
-
-}
